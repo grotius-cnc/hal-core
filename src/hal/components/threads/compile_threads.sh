761adcfaef08536c34c74fa935ba88365089a4b7
jjrbfi <71790456+jjrbfi@users.noreply.github.com>
2021-08-19 12:21:42 +0300

    Removed mieee-fp to be able to compile in Rpi

--- src/hal/components/threads/compile_threads.sh
+++ src/hal/components/threads/compile_threads.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Compile to .o file
-gcc -o threads.o -Os -g -I. -I/opt/hal-core/src/rtapi -I/opt/hal-core/src/hal -DUSPACE -fno-fast-math -mieee-fp -fno-unsafe-math-optimizations -DRTAPI -D_GNU_SOURCE -Drealtime -D__MODULE__ -I/opt/hal-core/src/rtapi -I/opt/hal-core/src/hal -DSIM -fPIC -Os -c threads.c
+gcc -o threads.o -Os -g -I. -I/opt/hal-core/src/rtapi -I/opt/hal-core/src/hal -DUSPACE -fno-fast-math -fno-unsafe-math-optimizations -DRTAPI -D_GNU_SOURCE -Drealtime -D__MODULE__ -I/opt/hal-core/src/rtapi -I/opt/hal-core/src/hal -DSIM -fPIC -Os -c threads.c
 
 # Compile to .so file
 gcc -shared -o threads.so threads.o  -Wl,-rpath,/opt/hal-core/lib -L/opt/hal-core/lib -lhalcore -lrt -I/opt/hal-core/src/rtapi/
