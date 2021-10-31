3ca8c2e8772fd3b02d4ba8a6bd63ade24ac69a6a
grotius-cnc <44880102+grotius-cnc@users.noreply.github.com>
2021-10-31 19:04:10 -0400

    Add files via upload
    update files

--- src/hal/components/dofs/hal/halmodule.c
+++ src/hal/components/dofs/hal/halmodule.c
@@ -26,19 +26,24 @@ skynet_t *skynet;
 typedef struct {
     hal_float_t *Pin;
 } float_data_t;
-float_data_t array_interfacetype[16];
-float_data_t array_current_pos[16];
-float_data_t array_current_vel[16];
-float_data_t array_current_acc[16];
-float_data_t array_target_pos[16];
-float_data_t array_maxvel[16];
-float_data_t array_maxacc[16];
-float_data_t array_maxjerk[16];
+float_data_t *array_maxvel[16];
+float_data_t *array_maxacc[16];
+float_data_t *array_maxjerk[16];
+float_data_t *array_interfacetype[16];
+float_data_t *array_current_pos[16];
+float_data_t *array_current_vel[16];
+float_data_t *array_current_acc[16];
+float_data_t *array_target_pos[16];
+
+// Stepgen
+float_data_t *cycle_delay[16], *pos_cmd[16], *pos_fb[16], *pulses_mm[16];
 
 typedef struct {
     hal_bit_t *Pin;
 } bit_data_t;
-bit_data_t array_enable[16];
+bit_data_t *array_enable[16];
+// Stepgen
+bit_data_t *array_step[16], *array_dir[16], *array_error[16];
 
 typedef struct {
     hal_s32_t *Pin;
@@ -63,8 +68,15 @@ typedef struct {
 static int comp_idx;
 
 static void function();
+static void pulse();
+static void control();
 static int setup_pins();
 char base[20]="dofs.";
+
+// Stepgen
+int counter[16], pulsecounter[16], pulsecontrolcounter[16];
+bool on[16]; // StepHigh, StepLow
+float deltapos[16]; // DistanceToGo every 1ms.
 
 int rtapi_app_main(void) {
 
@@ -73,6 +85,7 @@ int rtapi_app_main(void) {
 
     int r = 0;
     r = hal_export_funct("dofs.run", function, &skynet,0,0,comp_idx);
+    r = hal_export_funct("dofs.pulse", pulse, &skynet,0,0,comp_idx);
     r+=setup_pins();
 
     if(r) {
@@ -80,6 +93,7 @@ int rtapi_app_main(void) {
     } else {
         hal_ready(comp_idx);
     }
+
     return 0;
 }
 
@@ -87,35 +101,95 @@ void rtapi_app_exit(void){
     hal_exit(comp_idx);
 }
 
-//! Perforn's every ms.
+//! Perforn's every servo-period 1ms.
 static void function(){
 
-        // For every DoFs perform :
-        for(unsigned int i=0; i<nr; i++){
+    // For every DoFs perform :
+    for(unsigned int i=0; i<nr; i++){
 
-            // Init state
-            struct result r;
-            r.curpos=*array_current_pos[i].Pin;
-            r.curvel=*array_current_vel[i].Pin;
-            r.curacc=*array_current_acc[i].Pin;
-            r.maxvel=*array_maxvel[i].Pin;
-            r.maxacc=*array_maxacc[i].Pin;
-            r.maxjerk=*array_maxjerk[i].Pin;
-            r.tarpos=*array_target_pos[i].Pin;
+        // Init state
+        struct result r;
+        r.curpos=*array_current_pos[i]->Pin;
+        r.curvel=*array_current_vel[i]->Pin;
+        r.curacc=*array_current_acc[i]->Pin;
+        r.maxvel=*array_maxvel[i]->Pin;
+        r.maxacc=*array_maxacc[i]->Pin;
+        r.maxjerk=*array_maxjerk[i]->Pin;
+        r.tarpos=*array_target_pos[i]->Pin;
 
-            r.enable=*array_enable[i].Pin;
-            r.interfacetype=0; // Enum index.
-            r.synchronizationtype=3;
-            r.durationdiscretizationtype=0;
+        r.enable=*array_enable[i]->Pin;
+        r.interfacetype=0; // Enum index.
+        r.synchronizationtype=3;
+        r.durationdiscretizationtype=0;
 
-            // Calculate
-            r=wrapper_get_pos_array(r);
+        // Calculate
+        r=wrapper_get_pos_array(r);
 
-            // Update state
-            *array_current_pos[i].Pin=r.curpos;
-            *array_current_vel[i].Pin=r.curvel;
-            *array_current_acc[i].Pin=r.curacc;
+        // Update state
+        *array_current_pos[i]->Pin=r.curpos;
+        *array_current_vel[i]->Pin=r.curvel;
+        *array_current_acc[i]->Pin=r.curacc;
+
+        pulsecounter[i]=0;
+
+        deltapos[i]=r.curpos-*pos_fb[i]->Pin;
+
+        // Example, travel 5mm in 1ms.
+        // Stepperdriver settings : 5000 pulse/rev
+        // 5000 pulse/(travel 40mm a revolution) = 125 pulse/mm * 5 mm travel = 625 pulses.
+
+        pulsecounter[i]=*pulses_mm[i]->Pin*deltapos[i];
+    }
+}
+
+//! Perforn's every base-thread period.
+static void pulse(){
+    // 1 millisecond = 1000 microseconds = 1000000 nanoseconds us
+
+    // For every DoFs perform :
+    for(unsigned int i=0; i<nr; i++){
+
+        if(pulsecounter[i]>0){
+            *array_dir[i]->Pin=0;
+
+            counter[i]++;
+            if(on[i] && counter[i]>*cycle_delay[i]->Pin){
+                *array_step[i]->Pin=1;
+                on[i]=0;
+                counter[i]=0;
+
+                pulsecounter[i]--;
+                pulsecontrolcounter[i]++;
+
+            } else if(!on[i] && counter[i]>*cycle_delay[i]->Pin){
+                *array_step[i]->Pin=0;
+                on[i]=1;
+                counter[i]=0;
+            }
         }
+
+        if(pulsecounter[i]<0){
+            *array_dir[i]->Pin=1;
+
+            counter[i]++;
+            if(on[i] && counter[i]>*cycle_delay[i]->Pin){
+                *array_step[i]->Pin=1;
+                on[i]=0;
+                counter[i]=0;
+
+                pulsecounter[i]++;
+                pulsecontrolcounter[i]--;
+
+            } else if(!on[i] && counter[i]>*cycle_delay[i]->Pin){
+                *array_step[i]->Pin=0;
+                on[i]=1;
+                counter[i]=0;
+            }
+
+        }
+
+        *pos_fb[i]->Pin=pulsecontrolcounter[i]/ *pulses_mm[i]->Pin;
+    }
 }
 
 static int setup_pins(){
@@ -127,11 +201,8 @@ static int setup_pins(){
         char charvalue[3];
         sprintf(charvalue,"%d",i);
         strcat(name,charvalue);
-        bit_data_t *dofs_bit;
-        dofs_bit=(bit_data_t*)hal_malloc(sizeof(bit_data_t));
-        r+=hal_pin_bit_new(name,HAL_IN,&(dofs_bit->Pin),comp_idx);
-
-        array_enable[i].Pin=dofs_bit->Pin;
+        array_enable[i]=(bit_data_t*)hal_malloc(sizeof(bit_data_t));
+        r+=hal_pin_bit_new(name,HAL_IN,&(array_enable[i]->Pin),comp_idx);
     }
 
     for(unsigned int i=0; i<nr; i++){
@@ -139,11 +210,8 @@ static int setup_pins(){
         char charvalue[3];          // contains the int value up to 999.
         sprintf(charvalue,"%d",i);  // express a int to char.
         strcat(name,charvalue);     // assign text a + text b.
-        float_data_t *dofs_float;
-        dofs_float=(float_data_t*)hal_malloc(sizeof(float_data_t));
-        r+=hal_pin_float_new(name,HAL_OUT,&(dofs_float->Pin),comp_idx);
-
-        array_current_pos[i].Pin=dofs_float->Pin;
+        array_current_pos[i]=(float_data_t*)hal_malloc(sizeof(float_data_t));
+        r+=hal_pin_float_new(name,HAL_OUT,&(array_current_pos[i]->Pin),comp_idx);
     }
 
     for(unsigned int i=0; i<nr; i++){
@@ -151,11 +219,8 @@ static int setup_pins(){
         char charvalue[3];          // contains the int value up to 999.
         sprintf(charvalue,"%d",i);  // express a int to char.
         strcat(name,charvalue);     // assign text a + text b.
-        float_data_t *dofs_float;
-        dofs_float=(float_data_t*)hal_malloc(sizeof(float_data_t));
-        r+=hal_pin_float_new(name,HAL_OUT,&(dofs_float->Pin),comp_idx);
-
-        array_current_vel[i].Pin=dofs_float->Pin;
+        array_current_vel[i]=(float_data_t*)hal_malloc(sizeof(float_data_t));
+        r+=hal_pin_float_new(name,HAL_OUT,&(array_current_vel[i]->Pin),comp_idx);
     }
 
     for(unsigned int i=0; i<nr; i++){
@@ -163,11 +228,8 @@ static int setup_pins(){
         char charvalue[3];          // contains the int value up to 999.
         sprintf(charvalue,"%d",i);  // express a int to char.
         strcat(name,charvalue);     // assign text a + text b.
-        float_data_t *dofs_float;
-        dofs_float=(float_data_t*)hal_malloc(sizeof(float_data_t));
-        r+=hal_pin_float_new(name,HAL_OUT,&(dofs_float->Pin),comp_idx);
-
-        array_current_acc[i].Pin=dofs_float->Pin;
+        array_current_acc[i]=(float_data_t*)hal_malloc(sizeof(float_data_t));
+        r+=hal_pin_float_new(name,HAL_OUT,&(array_current_acc[i]->Pin),comp_idx);
     }
 
     for(unsigned int i=0; i<nr; i++){
@@ -175,11 +237,8 @@ static int setup_pins(){
         char charvalue[3];
         sprintf(charvalue,"%d",i);
         strcat(name,charvalue);
-        float_data_t *dofs_float;
-        dofs_float=(float_data_t*)hal_malloc(sizeof(float_data_t));
-        r+=hal_pin_float_new(name,HAL_IN,&(dofs_float->Pin),comp_idx);
-
-        array_target_pos[i].Pin=dofs_float->Pin;
+        array_target_pos[i]=(float_data_t*)hal_malloc(sizeof(float_data_t));
+        r+=hal_pin_float_new(name,HAL_IN,&(array_target_pos[i]->Pin),comp_idx);
     }
 
     for(unsigned int i=0; i<nr; i++){
@@ -187,11 +246,8 @@ static int setup_pins(){
         char charvalue[3];
         sprintf(charvalue,"%d",i);
         strcat(name,charvalue);
-        float_data_t *dofs_float;
-        dofs_float=(float_data_t*)hal_malloc(sizeof(float_data_t));
-        r+=hal_pin_float_new(name,HAL_IN,&(dofs_float->Pin),comp_idx);
-
-        array_maxvel[i].Pin=dofs_float->Pin;
+        array_maxvel[i]=(float_data_t*)hal_malloc(sizeof(float_data_t));
+        r+=hal_pin_float_new(name,HAL_IN,&(array_maxvel[i]->Pin),comp_idx);
     }
 
     for(unsigned int i=0; i<nr; i++){
@@ -199,11 +255,8 @@ static int setup_pins(){
         char charvalue[3];
         sprintf(charvalue,"%d",i);
         strcat(name,charvalue);
-        float_data_t *dofs_float;
-        dofs_float=(float_data_t*)hal_malloc(sizeof(float_data_t));
-        r+=hal_pin_float_new(name,HAL_IN,&(dofs_float->Pin),comp_idx);
-
-        array_maxacc[i].Pin=dofs_float->Pin;
+        array_maxacc[i]=(float_data_t*)hal_malloc(sizeof(float_data_t));
+        r+=hal_pin_float_new(name,HAL_IN,&(array_maxacc[i]->Pin),comp_idx);
     }
 
     for(unsigned int i=0; i<nr; i++){
@@ -211,12 +264,64 @@ static int setup_pins(){
         char charvalue[3];
         sprintf(charvalue,"%d",i);
         strcat(name,charvalue);
-        float_data_t *dofs_float;
-        dofs_float=(float_data_t*)hal_malloc(sizeof(float_data_t));
-        r+=hal_pin_float_new(name,HAL_IN,&(dofs_float->Pin),comp_idx);
-
-        array_maxjerk[i].Pin=dofs_float->Pin;
+        array_maxjerk[i]=(float_data_t*)hal_malloc(sizeof(float_data_t));
+        r+=hal_pin_float_new(name,HAL_IN,&(array_maxjerk[i]->Pin),comp_idx);
     }
+
+    for(unsigned int i=0; i<nr; i++){
+        char name[30]="dofs.stepgen.pulses_mm.";
+        char charvalue[3];
+        sprintf(charvalue,"%d",i);
+        strcat(name,charvalue);
+        pulses_mm[i]=(float_data_t*)hal_malloc(sizeof(float_data_t));
+        r+=hal_pin_float_new(name,HAL_IN,&(pulses_mm[i]->Pin),comp_idx);
+    }
+
+    for(unsigned int i=0; i<nr; i++){
+        char name[30]="dofs.stepgen.cycle_delay.";
+        char charvalue[3];
+        sprintf(charvalue,"%d",i);
+        strcat(name,charvalue);
+        cycle_delay[i]=(float_data_t*)hal_malloc(sizeof(float_data_t));
+        r+=hal_pin_float_new(name,HAL_IN,&(cycle_delay[i]->Pin),comp_idx);
+    }
+
+    for(unsigned int i=0; i<nr; i++){
+        char name[30]="dofs.stepgen.pos_fb.";
+        char charvalue[3];
+        sprintf(charvalue,"%d",i);
+        strcat(name,charvalue);
+        pos_fb[i]=(float_data_t*)hal_malloc(sizeof(float_data_t));
+        r+=hal_pin_float_new(name,HAL_IN,&(pos_fb[i]->Pin),comp_idx);
+    }
+
+    for(unsigned int i=0; i<nr; i++){
+        char name[20]="dofs.stepgen.step.";
+        char charvalue[3];
+        sprintf(charvalue,"%d",i);
+        strcat(name,charvalue);
+        array_step[i]=(bit_data_t*)hal_malloc(sizeof(bit_data_t));
+        r+=hal_pin_bit_new(name,HAL_OUT,&(array_step[i]->Pin),comp_idx);
+    }
+
+    for(unsigned int i=0; i<nr; i++){
+        char name[20]="dofs.stepgen.dir.";
+        char charvalue[3];
+        sprintf(charvalue,"%d",i);
+        strcat(name,charvalue);
+        array_dir[i]=(bit_data_t*)hal_malloc(sizeof(bit_data_t));
+        r+=hal_pin_bit_new(name,HAL_OUT,&(array_dir[i]->Pin),comp_idx);
+    }
+
+    for(unsigned int i=0; i<nr; i++){
+        char name[20]="dofs.stepgen.error.";
+        char charvalue[3];
+        sprintf(charvalue,"%d",i);
+        strcat(name,charvalue);
+        array_error[i]=(bit_data_t*)hal_malloc(sizeof(bit_data_t));
+        r+=hal_pin_bit_new(name,HAL_OUT,&(array_error[i]->Pin),comp_idx);
+    }
+
     return r;
 }
 
